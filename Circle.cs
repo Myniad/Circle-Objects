6a37890b18979881aefa48e41877c540c4429031
Junyoung Kim <jkim5929@gmail.com>
2023-01-23 16:11:35 -0500

    First Commit

---
+++ Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+            Console.WriteLine(CalculateFormattedCircumference());
+            Console.WriteLine(CalculateFormattedArea());
+
+        }
+
+
+        //methods
+        public double CalculateCircumference()
+        {
+            return 2 * (Math.PI * Radius);
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double ValueCirc = CalculateCircumference();
+            return $"Circumference: {FormatNumber(ValueCirc)}";
+        }
+        public double CalculateArea()
+        {
+            return (Math.PI * Radius*Radius);
+        }
+        public string CalculateFormattedArea()
+        {
+            double ValueArea = CalculateArea();
+            return $"Area: {FormatNumber(ValueArea)}";
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Circle_Objects;
+bool runProgram = true;
+double runCounter = 2;
+
+string value = "";
+Console.WriteLine("Welcome to the Circle Tester");
+
+while (runProgram)
+{
+    double radius = 0;
+    while (radius <= 0)
+    {
+        Console.Write("Enter radius: ");
+        while (double.TryParse(Console.ReadLine().Trim(), out radius) == false)
+        {
+            Console.WriteLine("Not a number, Try again.");
+        }
+       if (radius< 0)
+        {
+            Console.WriteLine("Number too low, Try again.");
+        }
+    }
+
+
+        Circle myCircle = new Circle(radius)
+        {
+            Radius = radius
+        };
+
+        while (true)
+        {
+            Console.Write("Continue? (y/n) ");
+            string continueCheck = Console.ReadLine().ToLower().Trim();
+            if (continueCheck == "y")
+            {
+                runProgram = true;
+                runCounter +=2;
+                break;
+            }
+            else if (continueCheck == "n")
+            {
+                runProgram = false;
+                Console.WriteLine($"Goodbye, you created {runCounter} Circle object(s)");
+                break;
+            }
+
+
+        }
+}
+
